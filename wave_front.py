fcebb4665bf91ea61dc755f4d90aca7649aa491e
jamie <neworderofjamie@gmail.com>
2016-06-29 14:59:01 +0100

    delay-based wavefront model

---
+++ wave_front.py
@@ -0,0 +1,218 @@
+import itertools
+import logging
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import numpy as np
+from scipy import misc
+
+spinnaker = False
+
+def get_neuron_index(x, y, width):
+    return (x * width) + y
+
+def add_connection(start_x, start_y,
+                   end_x, end_y,
+                   cost_image, conn_list):
+    # Get costs
+    start_cost = cost_image[start_y, start_x]
+    end_cost = cost_image[end_y, end_x]
+
+    # If end vertex isn't blocking
+    if end_cost != 0xFF:
+        # Convert average cost of the two vertices to delay
+        delay = 1.0 + ((float(start_cost) + float(end_cost)) * 0.5 * (7.0 / 256.0))
+
+        # Add connection
+        conn_list.append((get_neuron_index(start_x, start_y, cost_image.shape[0]),
+                            get_neuron_index(end_x, end_y, cost_image.shape[0]),
+                            weight, delay))
+
+if spinnaker:
+    import pynn_spinnaker as sim
+
+    logger = logging.getLogger("pynn_spinnaker")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+
+    setup_kwargs = { "spinnaker_hostname" : "192.168.240.253" }
+else:
+    import pyNN.nest as sim
+
+    setup_kwargs = {"spike_precision": "on_grid"}
+
+# Load cost image
+cost_image = misc.imread("map.png")
+
+# Where to start wave front
+stim_x = 43
+stim_y = 43
+
+# Where is our destination
+end_x = 10
+end_y = 10
+
+# How long to simulate
+duration = 500
+
+# What weight should we use to cause spikes
+# **NOTE** this is massive so single presynaptic spikes causes immediate postsynaptic spike
+weight = 30.0
+
+# setup simulator
+sim.setup(timestep=1.0, min_delay=1.0, max_delay=8.0, **setup_kwargs)
+
+# Create population of neurons
+num_neurons = cost_image.shape[0] * cost_image.shape[1]
+neurons = sim.Population(num_neurons, sim.IF_curr_exp(tau_refrac=30), label="pop")
+
+# Record spikes
+neurons.record("spikes")
+
+# Loop through neurons
+conn_list = []
+for x, y in itertools.product(range(cost_image.shape[0]),
+                              range(cost_image.shape[1])):
+    start_cost = cost_image[y, x]
+    if start_cost == 0xFF:
+        continue
+
+    # Left
+    if x > 0:
+        add_connection(x, y,
+                       x - 1, y,
+                       cost_image, conn_list)
+
+    # Right
+    if x < (cost_image.shape[0] - 1):
+        add_connection(x, y,
+                       x + 1, y,
+                       cost_image, conn_list)
+
+    # Up
+    if y > 0:
+        add_connection(x, y,
+                       x, y - 1,
+                       cost_image, conn_list)
+
+    # Down
+    if y < (cost_image.shape[1] - 1):
+        add_connection(x, y,
+                       x, y + 1,
+                       cost_image, conn_list)
+
+# Create connector
+sim.Projection(neurons, neurons, sim.FromListConnector(conn_list),
+               sim.StaticSynapse(),
+               receptor_type="excitatory")
+
+# Stimulate stim neuron
+stim = sim.Population(1, sim.SpikeSourceArray(spike_times=[2.0]), label="stim")
+sim.Projection(stim, neurons,
+               sim.FromListConnector([(0, get_neuron_index(stim_x, stim_y, cost_image.shape[0]), weight, 1.0)]),
+               sim.StaticSynapse())
+
+# Run network
+sim.run(duration)
+
+# Read data
+data = neurons.get_data()
+
+sim.end()
+
+# Convert spiketrains to matrix
+end_time = 0
+matrix = np.zeros((cost_image.shape[0], cost_image.shape[1], duration), dtype=bool)
+for spiketrain in data.segments[0].spiketrains:
+    # Get x and y coordinates
+    neuron_index = spiketrain.annotations["source_index"]
+    neuron_x = neuron_index % cost_image.shape[0]
+    neuron_y = neuron_index // cost_image.shape[0]
+
+    # Update end time
+    if len(spiketrain) > 0:
+        end_time = max(end_time, np.amax(spiketrain))
+
+    # Set spikes to 1
+    for t in np.asarray(spiketrain):
+        matrix[neuron_x, neuron_y, t - 1] = True
+
+print "End time:%u" % end_time
+
+# Create image to display path information
+path_image = np.zeros((cost_image.shape[0], cost_image.shape[1], 3))
+
+# Add pixels indicating stim and end to image
+path_image[stim_x, stim_y] = (0.0, 1.0, 0.0)
+path_image[end_x,  end_y] = (0.0, 0.0, 1.0)
+
+# Backtrack to find path
+x = end_x
+y = end_y
+max_time = duration
+while True:
+     # If we've reached stimulus, stop
+    if x == stim_x and y == stim_y:
+        break;
+
+    # Loop through neighbours
+    first_time = max_time
+    first_x = None
+    first_y = None
+    for x_offset, y_offset in itertools.product(range(-1, 2, 1), repeat=2):
+        # Skip self-connections
+        if x_offset == 0 and y_offset == 0:
+            continue
+
+        # Get spike vector for this offset vertex
+        spike_vector = matrix[x + x_offset,y + y_offset,:]
+
+        # Find the first time bin in which it spiked
+        spike_time = np.where(spike_vector == True)[0]
+
+        # If there were any spikes at this pixel and the first spike
+        # that occured is earlier than current best
+        if len(spike_time) > 0 and spike_time[0] < first_time:
+            first_time = spike_time[0]
+            first_x = x + x_offset
+            first_y = y + y_offset
+
+    # Check parent is found
+    assert first_x is not None
+    assert first_y is not None
+
+    # Advance to parent
+    x = first_x
+    y = first_y
+    max_time = first_time
+
+    # Draw path point
+    path_image[x, y, :] = (1.0, 0.0, 0.0)
+
+fig, axis = plt.subplots()
+
+image = np.zeros((cost_image.shape[0], cost_image.shape[1]))
+image[:] = matrix[:,:,0]
+axis.imshow(cost_image, interpolation="nearest")
+axis.imshow(path_image, interpolation="nearest",
+            vmin=0.0, vmax=1.0, alpha=0.5)
+spike_image = axis.imshow(image, interpolation="nearest",
+                          vmin=0.0, vmax=1.0, alpha=0.5)
+
+def updatefig(frame):
+    global image
+
+    # Decay image
+    # **TODO** interval`
+    image *= 0.9
+
+    # Add this frame's spike vector to image
+    image += matrix[:,:,frame]
+
+    # Update image data
+    spike_image.set_array(image)
+    return [spike_image]
+
+# Play animation
+ani = anim.FuncAnimation(fig, updatefig, range(end_time), interval=30,
+                         blit=True, repeat=True)
+plt.show()
