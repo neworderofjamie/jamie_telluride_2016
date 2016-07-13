5797a8f92dc1d82b589b3fea94f00801fb29158e
jamie <neworderofjamie@gmail.com>
2016-07-13 22:29:53 +0100

    Jesus's demo

---
+++ cerebellum_learning_rule.py
@@ -0,0 +1,91 @@
+import pylab
+import spynnaker.pyNN as sim
+import cerebellum as cer
+
+# ------------------------------------------------------------------
+# This example uses the sPyNNaker implementation of cerebellar
+# STDP
+# ------------------------------------------------------------------
+
+# ------------------------------------------------------------------
+# Common parameters
+# ------------------------------------------------------------------
+teaching_time = 500
+delta_t = range(0,teaching_time,10)
+start_time = 200
+num_pre_cells = 100
+
+# Population parameters
+model = cer.IFCurrExpSupervision
+cell_params = {'cm': 0.25,  # nF
+               'i_offset': 0.0,
+               'tau_m': 10.0,
+               'tau_refrac': 2.0,
+               'tau_syn_E': 2.5,
+               'tau_syn_I': 2.5,
+               'v_reset': -70.0,
+               'v_rest': -65.0,
+               'v_thresh': -55.4
+               }
+
+# SpiNNaker setup
+sim.setup(timestep=0.1, min_delay=1.0, max_delay=10.0)
+
+
+sim_time = teaching_time + 100.0
+first_spike_time = 100.0
+pre_stim = []
+
+for delta in delta_t:
+    print first_spike_time + delta
+    pre_stim.append(sim.Population(1, sim.SpikeSourceArray,{'spike_times': [first_spike_time+delta, ]}))
+
+
+teaching_stim = sim.Population(1, sim.SpikeSourceArray,{'spike_times': [teaching_time, ]})
+
+
+# Neuron populations
+population = sim.Population(1, model, cell_params)
+
+
+ # Plastic Connection between pre_pop and post_pop
+stdp_model = sim.STDPMechanism(
+    timing_dependence = cer.TimingDependenceCerebellum(tau=20.0),
+    weight_dependence = sim.AdditiveWeightDependence(w_min=0.0, w_max=1.0, A_plus=0.1, A_minus=0.1)
+)
+
+# Connections between spike sources and neuron populations
+projections_pf = []
+for stim in pre_stim:
+    ####### SET HERE THE PARALLEL FIBER-PURKINJE CELL LEARNING RULE
+    ee_connector = sim.OneToOneConnector(weights=0.5)
+    projections_pf.append(sim.Projection(stim, population, ee_connector,
+                                         synapse_dynamics=sim.SynapseDynamics(slow=stdp_model),
+                                         target='excitatory'))
+
+# SET HERE THE TEACHING SIGNAL PROJECTION
+ee_connector = sim.OneToOneConnector()
+proj_teaching = sim.Projection(teaching_stim, population, ee_connector, target='supervision')
+
+print("Simulating for %us" % (sim_time / 1000))
+
+# Run simulation
+sim.run(sim_time)
+
+# Get weight from each projection
+end_w = [p.getWeights()[0] for p in projections_pf]
+
+
+# -------------------------------------------------------------------
+# Plot curve
+# -------------------------------------------------------------------
+# Plot STDP curve
+figure, axis = pylab.subplots()
+axis.set_xlabel('Time')
+axis.set_ylabel('Weight',rotation="horizontal", size="xx-large")
+axis.plot(delta_t, end_w)
+
+pylab.show()
+
+# End simulation on SpiNNaker
+sim.end()
