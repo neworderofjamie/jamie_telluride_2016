d71056b74f629d145ae35ce7b57e145cabeb69f2
jgarridoalcazar <jgarrido@jgarrido.local>
2016-08-07 12:57:02 +0200

    Updated python package and path in cerebellar learning rule
    python_models path/package has been changed to cerebellum (as the
    folder where the python codes are included).

--- cerebellum/cerebellum/setup.py
+++ cerebellum/cerebellum/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="cerebellum",
     version="0.1.0",
-    packages=['python_models',],
-    package_data={'python_models.model_binaries': ['*.aplx']},
+    packages=['cerebellum',],
+    package_data={'cerebellum.model_binaries': ['*.aplx']},
     install_requires=['SpyNNaker == 2016.001']
 )
